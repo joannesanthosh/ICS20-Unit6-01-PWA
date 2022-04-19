60c7c279d4176cc9410fa4185d60f673c204aacc
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-04-19 13:21:21 -0400

    change 9 files

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+/**
+ * Check servie worker.
+ */
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
 
 /**
  * This function converts the degrees from fahrenheit to celsius
