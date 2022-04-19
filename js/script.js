dae25a2d6e1b955c3277e935318fed99f060e5e9
joannesanthosh <joannesanthosh@users.noreply.github.com>
2022-04-19 17:23:03 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -7,23 +7,23 @@
 /**
  * Check servie worker.
  */
- if (navigator.serviceWorker) {
+if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
     scope: "/ICS2O-PWA-Test/",
   })
 }
-
 
 /**
  * This function converts the degrees from fahrenheit to celsius
  */
 function convert() {
   // input
-  const fahrenheit = parseInt(document.getElementById('fahrenheit').value);
+  const fahrenheit = parseInt(document.getElementById("fahrenheit").value)
 
   // process
-  const celsius = (fahrenheit - 32) * 5 / 9
+  const celsius = ((fahrenheit - 32) * 5) / 9
 
   // output
-  document.getElementById('celsius').innerHTML = '<p>The  temperature in celsius is: ' + celsius.toFixed(2) + '°<p>'
-  }
+  document.getElementById("celsius").innerHTML =
+    "<p>The  temperature in celsius is: " + celsius.toFixed(2) + "°<p>"
+}
